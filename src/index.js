f142ffdf74012c53f363e1b8c63d84b5e105c700
Сергей Тынский <tynskysergei@yandex.ru>
2024-11-14 17:15:40 +0300

    Донастройка проекта

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import './pages/index.css';
+
+const numbers = [2, 3, 5];
+
+const doubledNumbers = numbers.map(number => number * 2);
+
+console.log(doubledNumbers);
--- trans-logistic/src/index.js
+++
@@ -1,4 +0,0 @@
-import {test} from "./components/validation"
-
-test();
-console.log("Точно")
